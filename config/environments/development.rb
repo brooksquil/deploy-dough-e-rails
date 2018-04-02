0a93940c3fbb1edcfdbf9b1d9dc84dee34a3af23
Brooke Wittenberg <brooke.wittenberg@gmail.com>
2018-04-01 22:08:49 -0500

    added paperclip path to imagemagik

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  
+  # give paperclip access to imagemagik
+  Paperclip.options[:command_path] = "/usr/local/bin/"
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
