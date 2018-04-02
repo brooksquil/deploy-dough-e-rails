97fc2898d1c34e5b925d6906333731e26143e817
Brooke Wittenberg <brooke.wittenberg@gmail.com>
2018-04-01 22:02:36 -0500

    added paperclip to gem file and product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180331014757) do
+ActiveRecord::Schema.define(version: 20180402025903) do
 
   create_table "companies", force: :cascade do |t|
     t.text "company_name"
@@ -85,6 +85,10 @@ ActiveRecord::Schema.define(version: 20180331014757) do
     t.integer "company_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["company_id"], name: "index_products_on_company_id"
     t.index ["product_type_id"], name: "index_products_on_product_type_id"
     t.index ["quantity_type_id"], name: "index_products_on_quantity_type_id"
