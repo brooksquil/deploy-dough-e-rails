97f95ad297d31d92e75df88cca1f3d4fe25d7cf0
Brooke Wittenberg <brooke.wittenberg@gmail.com>
2018-04-01 23:37:43 -0500

    added auth logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks 
   protect_from_forgery with: :exception
+    
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+    helper_method :current_user
+  
+    def authorize
+      redirect_to '/login' unless current_user
+    end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to products_url, :alert => exception.message
+  end
+  
 end
+
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+    def new
+    end
+  
+    def create
+      user = User.find_by(email: params[:email])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        if user.admin?
+          redirect_to todays_production_url, notice: 'Logged in!'
+        else
+        redirect_to products_url, notice: 'Logged in!' and return
+        end
+      else
+        flash.now.alert = 'Email or password is invalid'
+        render :new
+      end
+    end
+  
+    def destroy
+      session[:user_id] = nil
+      redirect_to root_url, notice: 'Logged out!'
+    end
+end
+  
+      
+  
